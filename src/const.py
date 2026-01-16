8fdfd260ca3c95b04ae4c568adfb2a89c2757e10
ZxShen <77840381+FrankZxShen@users.noreply.github.com>
2026-01-17 00:43:31 +0800

    Update constants for OpenAI API and model paths
    Commented out unused API endpoint and key variables, and added a new model name.

--- src/const.py
+++ src/const.py
@@ -1,6 +1,28 @@
 # about habitat scene
+from pyexpat import model
+from re import M
+
+
 INVALID_SCENE_ID = []
 
 # about chatgpt api
-END_POINT = ""
-OPENAI_KEY = ""
+# END_POINT = ""
+# OPENAI_KEY = ""
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:31512/v1"
+# MODEL_NAME = "/home/szx/DATACENTER1/szx/models/LLMs/Qwen3-VL-4B-Instruct"
+# MODEL_NAME = "/home/szx/DATACENTER1/szx/models/LLMs/Qwen3-VL-8B-Instruct"
+MODEL_NAME = "/home/szx/project/NavEvolver_v2/DATA/results/global_step_1415/actor/huggingface"
+# MODEL_NAME = "/home/szx/project/NavEvolver_v2/DATA/results/global_step_1365-300data/actor/huggingface"
+# MODEL_NAME = "/home/szx/DATACENTER1/szx/models/hf/hub/models--Qwen--Qwen3-VL-2B-Instruct/snapshots/89644892e4d85e24eaac8bacfd4f463576704203" #qwen-vl-2b
+# MODEL_NAME = "/home/szx/project/3D-Mem-Debug/results/results/eta0.0" #qwen-vl-2b
+# MODEL_NAME = "/home/szx/project/3D-Mem-Debug/results/results/epsilon0.8" #qwen-vl-2b
+# MODEL_NAME = "/home/szx/project/3D-Mem-Debug/results/results/epsilon1.2" #qwen-vl-2b
+# MODEL_NAME = "/home/szx/project/3D-Mem-Debug/results/results/Qwen3-vl-4b-train1/qwen3-vl-4b-nav-v1" #qwen-vl-4b
+# MODEL_NAME = "/home/szx/project/3D-Mem-Debug/results/results/Qwen3-vl-4b-v2" #qwen-vl-4b
+# MODEL_NAME = "/home/szx/project/3D-Mem-Debug/results/results/100data" #qwen-vl-4b
+# MODEL_NAME = '/home/szx/project/3D-Mem-Debug/results/results/data400'
+# MODEL_NAME = "/home/szx/project/3D-Mem-Debug/results/results/3images"
+
+
+# MODEL_NAME = "/mnt/disk/shengzhixuan/LLMs/Qwen2.5-VL-7B-Instruct"  # "gpt-4o"  # "gpt-4o"  # "gpt-4o-mini
