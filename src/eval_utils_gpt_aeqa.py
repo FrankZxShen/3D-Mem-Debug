6c7645208924cd103ae5bbf1aa4a4eeae1010257
ZxShen <77840381+FrankZxShen@users.noreply.github.com>
2026-01-29 00:39:05 +0800

    Update eval_utils_gpt_aeqa.py

--- src/eval_utils_gpt_aeqa.py
+++ src/eval_utils_gpt_aeqa.py
@@ -18,14 +18,7 @@ from src.const import *
 #     api_key=OPENAI_KEY,
 # )
 
-# For qwen3-vl-plus
-# client = OpenAI(
-#     # 若没有配置环境变量，请用阿里云百炼API Key将下行替换为：api_key="sk-xxx",
-#     # 新加坡和北京地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
-#     api_key="sk-44e9a14de5534e71963237a21e277b30",
-#     # 以下是北京地域base_url，如果使用新加坡地域的模型，需要将base_url替换为：https://dashscope-intl.aliyuncs.com/compatible-mode/v1
-#     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-# )
+
 
 # For local VLM
 client = OpenAI(
